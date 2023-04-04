b2942105dd9f88bc7b7f71e00aeecab0db11d143
Maxim <maximyurinmusic@gmail.com>
2023-04-04 20:01:25 +0300

    Homework is finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,149 @@
-function sum(a, b) {
-  return a + b;
+// Функции 1-го блока
+
+/**
+ * Функция расчёта суммы чисел. Функция проверяют чтобы тип введённых аргументов был number.
+ * @param num1 <-- Первый аргумент функции, тип number.
+ * @param num2 <-- Второй аргумент функции, тип number.
+ * @returns {void} <-- Функция возвращает сумму переданых аргументов.
+ */
+function sum(num1, num2) {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  }
+  return reportError(`Одно из значений не является числом!!!!`);
+}
+
+/**
+ * Функция расчёта разницы чисел. Функция проверяют чтобы тип введённых аргументов был number.
+ * @param num1 <-- Первый аргумент функции, тип number.
+ * @param num2 <-- Второй аргумент функции, тип number.
+ * @returns {void} <-- Функция возвращает разницу переданых аргументов.
+ */
+function diff(num1, num2) {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 - num2;
+  }
+  reportError(`Одно из значений не является числом!!!!`);
+}
+
+/**
+ * Функция расчёта умножения чисел. Функция проверяют чтобы тип введённых аргументов был number.
+ * @param num1 <-- Первый аргумент функции, тип number.
+ * @param num2 <-- Второй аргумент функции, тип number.
+ * @returns {void} <-- Функция возвращает результат умножения аргументов.
+ */
+function multi(num1, num2) {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 * num2;
+  }
+  reportError(`Одно из значений не является числом!!!!`);
+}
+
+/**
+ * Функция расчёта деления чисел. Функция проверяют чтобы тип введённых аргументов был number.
+ * В случае если кторой аргумент будет равен 0, будет получена ошибка, так как на 0 делить нельзя.
+ * @param num1 <-- Первый аргумент функции, тип number.
+ * @param num2 <-- Второй аргумент функции, тип number.
+ * @returns {void} <-- Функция возвращает результат деления аргументов.
+ */
+function division(num1, num2) {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    if (num2 === 0) {
+      reportError(`На ноль делить нельзя, введите новое число!`);
+    }
+    return num1 / num2;
+  }
+  reportError(`Одно из значений не является числом!!!!`);
+}
+
+/**
+ * Функция ищет максимальное число в массиве
+ * @param array <-- Аргументом данной функции обязательно должен быть массив!
+ */
+function maxNumber(array) {
+  let maxNumberInTestArray = -Infinity;
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "number") {
+      if (array[i] > maxNumberInTestArray) {
+        maxNumberInTestArray = array[i];
+      }
+    }
+  }
+  return maxNumberInTestArray;
+}
+
+/**
+ * Функция ищет минимальное число в массиве
+ * @param array <-- Аргументом данной функции обязательно должен быть массив!
+ */
+function minNumber(array) {
+  let minNumberInTestArray = Infinity;
+  for (let element of array) {
+    if (typeof element === "number") {
+      if (element < minNumberInTestArray) {
+        minNumberInTestArray = element;
+      }
+    }
+  }
+  return minNumberInTestArray;
+}
+
+// Функции 2-го блока
+
+// Функция-конструктор Accumulator, включающая 2 собстенных метода increment и decrement.
+function Accumulator(initialValue) {
+  this.value = initialValue;
+}
+
+// Метод Increment
+Accumulator.prototype.increment = function () {
+  this.value++;
+};
+
+// Метод Decrement
+Accumulator.prototype.decrement = function () {
+  this.value--;
+};
+
+// Функция-конструктор CancelableAccumulator
+function CancelableAccumulator(initialValue) {
+  Accumulator.call(this, initialValue);
+  this.initialValue = initialValue;
+}
+
+// Прототипное наследование
+CancelableAccumulator.prototype = Object.create(Accumulator.prototype);
+
+// Метод Clear
+CancelableAccumulator.prototype.clear = function () {
+  this.value = this.initialValue;
+};
+
+// Функции 3-го блока
+
+// 1
+function calculateAverage(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+
+// 2
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+User.prototype.sayHello = function () {
+  return `Hi. My name is ${this.name} and I'm ${this.age} years old`;
+};
+
+// 3
+function calculator(init = 0) {
+  init++;
+  return function () {
+    return init++;
+  };
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,15 +1,219 @@
-describe("Sum of tow numbers", () => {
-    const desc = "<p>Sum of tow numbers</p>";
+describe("Sum of two numbers", () => {
+  const desc = "<p>Sum of two numbers</p>";
 
-    describe(desc, () => {
-        it("should return 5", () => {
-            const result = sum(2, 3);
-            expect(result).toBe(5);
-        });
-
-        it("should return 0", () => {
-            const result = sum(0, 1);
-            expect(result).toBe(0);
-        });
+  describe(desc, () => {
+    it("Sum should return 9", () => {
+      const result = sum(2, 7);
+      expect(result).toBe(9);
     });
+  });
+});
+
+describe("Difference of two numbers", () => {
+  const desc = "<p>Difference of two numbers</p>";
+
+  describe(desc, () => {
+    it("Difference should return 7", () => {
+      const result = diff(10, 3);
+      expect(result).toBe(7);
+    });
+  });
+});
+
+describe("Multiplication of two numbers", () => {
+  const desc = "<p>Multiplication of two numbers</p>";
+
+  describe(desc, () => {
+    it("Multiplication should return 39", () => {
+      const result = multi(13, 3);
+      expect(result).toBe(39);
+    });
+  });
+});
+
+describe("Division of two numbers", () => {
+  const desc = "<p>Division of two numbers</p>";
+
+  describe(desc, () => {
+    it("Division should return 11", () => {
+      const result = division(33, 3);
+      expect(result).toBe(11);
+    });
+  });
+});
+
+describe("Max number in array", () => {
+  const desc = "<p>Max number in array</p>";
+
+  describe(desc, () => {
+    it("should return 81", () => {
+      const testArray = [
+        5,
+        true,
+        64,
+        "Maxim",
+        81,
+        11,
+        32,
+        false,
+        32,
+        19,
+        2,
+        "string",
+        28,
+      ];
+      const result = maxNumber(testArray);
+      expect(result).toBe(81);
+    });
+  });
+});
+
+describe("Min number in array", () => {
+  const desc = "<p>Min number in array</p>";
+
+  describe(desc, () => {
+    it("should return 2", () => {
+      const testArray = [
+        5,
+        true,
+        64,
+        "Maxim",
+        81,
+        11,
+        32,
+        false,
+        32,
+        19,
+        2,
+        "string",
+        28,
+      ];
+      const result = minNumber(testArray);
+      expect(result).toBe(2);
+    });
+  });
+});
+
+describe("Accumulator constructor", () => {
+  const desc = "<p>Accumulator constructor</p>";
+
+  describe(desc, () => {
+    it("Initial value should return 28", () => {
+      const result = new Accumulator(28);
+      expect(result.value).toBe(28);
+    });
+  });
+});
+
+describe("Increment method of Accumulator", () => {
+  const desc = "<p>Increment method of Accumulator</p>";
+
+  describe(desc, () => {
+    it("Initial value is 28, Increment should return 29", () => {
+      const result = new Accumulator(28);
+      result.increment();
+      expect(result.value).toBe(29);
+    });
+  });
+});
+
+describe("Decrement method of Accumulator", () => {
+  const desc = "<p>Decrement method of Accumulator</p>";
+
+  describe(desc, () => {
+    it("Initial value is 44, double decrement should return 42", () => {
+      const result = new Accumulator(44);
+      result.decrement();
+      result.decrement();
+      expect(result.value).toBe(42);
+    });
+  });
+});
+
+describe("CancelableAccumulator constructor", () => {
+  const desc = "<p>CancelableAccumulator constructor</p>";
+
+  describe(desc, () => {
+    it("Initial value should return 30", () => {
+      const result = new CancelableAccumulator(30);
+      expect(result.value).toBe(30);
+    });
+  });
+});
+
+describe("CancelableAccumulator constructor", () => {
+  const desc = "<p>Inherited increment method of Accumulator constructor</p>";
+
+  describe(desc, () => {
+    it("Initial value is 30, double decrement should return 28", () => {
+      const result = new CancelableAccumulator(30);
+      result.decrement();
+      result.decrement();
+      expect(result.value).toBe(28);
+    });
+  });
+});
+
+describe("CancelableAccumulator constructor", () => {
+  const desc = "<p>Onw clear method of CancelableAccumulator constructor</p>";
+
+  describe(desc, () => {
+    it("Initial value is 25. Reset to the original input of initial value after 5 decrements should return 25", () => {
+      const result = new CancelableAccumulator(25);
+      result.decrement();
+      result.decrement();
+      result.decrement();
+      result.decrement();
+      result.decrement();
+      result.clear();
+      expect(result.value).toBe(25);
+    });
+  });
+});
+
+describe("Calculate Average numbers in array", () => {
+  const desc = "<p>Calculate Average numbers in array</p>";
+
+  describe(desc, () => {
+    it("Function should return 3", () => {
+      const myArray = [1, 2, 3, 4, 5];
+      const result = calculateAverage(myArray);
+      expect(result).toBe(3);
+    });
+  });
+});
+
+describe("User constructor", () => {
+  const desc = "<p>User constructor</p>";
+
+  describe(desc, () => {
+    it("Function should return 'Hi. My name is Sherlock and I'm 27 years old'", () => {
+      const user = new User("Sherlock", 27);
+      const result = user.sayHello();
+      expect(result).toBe(`Hi. My name is Sherlock and I'm 27 years old`);
+    });
+  });
+});
+
+describe("Calculator", () => {
+  const desc = "<p>Calculator</p>";
+
+  describe(desc, () => {
+    it("Function should return 1", () => {
+      const result = calculator();
+      expect(result()).toBe(1);
+    });
+  });
+});
+
+describe("Calculator", () => {
+  const desc = "<p>Calculator</p>";
+
+  describe(desc, () => {
+    it("Function should return 2", () => {
+      const result = calculator();
+      result();
+      expect(result()).toBe(2);
+    });
+  });
 });
